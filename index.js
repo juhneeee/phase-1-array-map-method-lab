077ab2a7045f818702d2f47c3268a4954d50d6a5
Johnny Chen <johnnyc1314@gmail.com>
2022-04-06 01:46:50 -0400

    commit tree 8b892be6f6d0d234178761369baeb68b8d1e5670

--- index.js
+++ index.js
@@ -12,5 +12,31 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  let newArr = tutorials.map(element => properCase(element))
+  return newArr
 }
+
+let properCase = (str) => {
+  let newArr = str.split(" ");
+  newArr = newArr.map((element)=> element[0].toUpperCase()+element.slice(1));
+  let newStr = newArr.join(' ')
+  return newStr
+}
+
+  // step 1 iterate through tutorials
+  // step 2 iterate through elements and capitalize strings 
+  // take strings and return titled strings
+  // step 3 how to title strings
+  // 3a convert strings into array with split
+  // 3b cap first letter for each element in array
+  // 3c convert the array back into a string
+
+
+
+
+// properCase = (str) => {
+//   let newArr = str.split(" ");
+//   newArr = newArr.map((element)=> element[0].toUpperCase()+element.slice(1));
+//   let newStr = newArr.join(' ')
+//   return newStr
+// }
